ded0cda488cc48712accd71ac2f1ec2fcfa831f0
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 06:18:43 +0300

    Proje dosyası ekle.

---
+++ StudyingMetho4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMetho4
+{
+    internal class Program
+    {
+        static void Addname(string lastName) {
+            Console.WriteLine("Zeynep" + " " +  lastName);
+        }
+
+        static void Main(string[] args)
+        {
+            Addname("Can");
+            Addname("Cankus");
+            Addname("Can Can");
+
+            Console.Read();
+        }
+    }
+}
